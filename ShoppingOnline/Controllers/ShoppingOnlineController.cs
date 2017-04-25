93c3d850871a918ba6c1d465c951ebb3a18d3824
Amorn Amorn <Amorn Amorn>
2017-04-25 22:51:40 +0700

    Upload

---
+++ ShoppingOnline/Controllers/ShoppingOnlineController.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingOnline.Models;
+
+namespace ShoppingOnline.Controllers
+{
+    public class ShoppingOnlineController : Controller
+    {
+        //
+        // GET: /ShoppingOnline/
+        DAO.ShoppingOnlineDAO dao = new DAO.ShoppingOnlineDAO();
+  
+        public ActionResult Login()
+        {
+            Session["CustomerID"] = null;
+            Session["ContactName"] = null;
+            Session["Status"] = null;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Customers memb)
+        {
+            MCVEntityes entity = new MCVEntityes();
+            var chk = entity.Customers.Where(x => x.ContactName == memb.ContactName && x.Password == memb.Password).FirstOrDefault();
+            if (chk != null)
+            {
+                Session["CustomerID"] = chk.CustomerID.ToString();
+                Session["ContactName"] = chk.ContactName.ToString();
+                Session["Status"] = chk.Status;
+                return RedirectToAction("Index");
+            }
+            return View(memb);
+        }
+      
+
+        [HttpPost]
+        public ActionResult insertProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult insertProduct(Models.Products why)
+        {
+            dao.insertProduct(why);
+            return RedirectToAction("Index", "ShoppingOnline");
+        }
+
+        public ActionResult Index()
+        {
+            return View(dao.Showcategories());
+        }
+        [HttpGet]
+        public ActionResult regiters()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult regiters(Models.Customers memb)
+        {
+            dao.save(memb);
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult deleteporduct(int id)
+        {
+            dao.Deleteproducts(id);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult deleteProduct(int id)
+        {
+            dao.Deletewhey(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult deletemem(int id)
+        {
+            dao.Deletemember(id);
+            return RedirectToAction("showmember");
+        }
+        public ActionResult deleteOrder(int id) {
+            dao.DeleteOrder(id);
+            return RedirectToAction("showorders");
+        }
+
+        public ActionResult deleteOrders(int id) {
+            dao.DeleteOrders(id);
+            return RedirectToAction("showorders2");
+        }
+
+
+
+        [HttpGet]
+        public ActionResult editporduct(int id)
+        {
+            ViewBag.list = Selectcategorise();
+            Models.Products prod = dao.Getproduct(id);
+            return View(prod);
+
+        }
+
+        [HttpPost]
+        public ActionResult editporduct(Models.Products prod)
+        {
+            dao.Update(prod);
+            return RedirectToAction("Index", "ShoppingOnline");
+        }
+
+
+        [HttpGet]
+        public ActionResult editwhey(int id)
+        {
+
+            Models.Products why = dao.GetProducts(id);
+            return View(why);
+
+        }
+
+        [HttpPost]
+        public ActionResult editwhey(Models.Products why)
+        {
+            dao.UpdatProduct(why);
+            return RedirectToAction("showwhey", "ShoppingOnline");
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult editmem(int id)
+        {
+
+            Models.Customers why = dao.Getmember(id);
+            return View(why);
+
+        }
+        [HttpPost]
+        public ActionResult editmem(Models.Customers mem)
+        {
+            dao.Updatemem(mem);
+            return RedirectToAction("showmember", "ShoppingOnline");
+        }
+
+        [HttpGet]
+        public ActionResult editbay(int id)
+        {
+            dao.Updatebay(id);
+            return RedirectToAction("showorders2");
+        }
+
+
+
+
+
+
+
+        public ActionResult insertproduct()
+        {
+            ViewBag.list = Selectcategorise();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult insertproduct(Models.Products prod)
+        {
+            dao.insertProduct(prod);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult showproducts(int id)
+        {
+            return View(dao.Showproducts(id));
+        }
+
+        public ActionResult showcategorise()
+        {
+            return View(dao.Showcategories());
+        }
+
+        public ActionResult showwhey(int id)
+        {
+            return View(dao.Showwheyprotein(id));
+        }
+
+        public ActionResult showmember()
+        {
+            return View(dao.Showmember());
+        }
+
+        public ActionResult showorders()
+        {
+            return View(dao.Showorders());
+        }
+
+        public ActionResult showorders2()
+        {
+            return View(dao.Showorders2());
+        }
+
+        [HttpPost]
+        public ActionResult showwhey(Models.Orders bays)
+        {
+            dao.insertbay(bays);
+            return RedirectToAction("Index");
+        }
+
+        public List<SelectListItem> Selectcategorise()
+        {
+            List<SelectListItem> listCategory = new List<SelectListItem>();
+            List<Categorys> list = dao.Getcategories();
+            foreach (Categorys cd in list)
+            {
+                SelectListItem item = new SelectListItem();
+                item.Text = cd.CategoryName;
+                item.Value = cd.CategoryID.ToString();
+                listCategory.Add(item);
+            }
+            return listCategory;
+        }
+	}
+}
---
+++ ShoppingOnline/DAO/ShoppingOnlineDAO.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoppingOnline.Models;
+
+namespace ShoppingOnline.DAO
+{
+   
+    public class ShoppingOnlineDAO
+    {
+        Models.MCVEntityes entity = new Models.MCVEntityes();
+
+        public void save(Models.Customers customer) {
+            entity.Customers.Add(customer);
+            entity.SaveChanges();
+        }
+        public void insertbay(Models.Orders bays)
+        {
+            entity.Orders.Add(bays);
+            entity.SaveChanges();
+        }
+
+        public void insertProduct(Models.Products why)
+        {
+            entity.Products.Add(why);
+            entity.SaveChanges();
+        }
+
+        public List<Categorys> Getcategories()
+        {
+            List<Categorys> list = new List<Categorys>();
+            list = entity.Categorys.ToList();
+            return list;
+        }
+
+        public List<Categorys> Showcategories()
+        {
+            List<Categorys> list = new List<Categorys>();
+            list = entity.Categorys.ToList();
+            return list;
+        }
+
+        public List<Products> Showwheyprotein(int id)
+        {
+            List<Products> list = new List<Products>();
+            list = entity.Products.Where(x => x.CategoryID == id).ToList();
+            return list;
+        }
+
+        public List<Customers> Showmember()
+        {
+            List<Customers> list = new List<Customers>();
+            list = entity.Customers.ToList();
+            return list;
+        }
+
+
+        public List<Products> Showproducts(int id)
+        {
+            List<Products> list = new List<Products>();
+            list = entity.Products.Where(x => x.CategoryID == id).ToList();
+            return list;
+        }
+
+        public List<Orders> Showorders()
+        {
+            List<Orders> list = new List<Orders>();
+            list = entity.Orders.Where(x => x.status_bay == 0).ToList();
+            return list;
+        }
+
+        public List<Orders> Showorders2()
+        {
+            List<Orders> list = new List<Orders>();
+            list = entity.Orders.Where(x => x.status_bay == 1).ToList();
+            return list;
+        }
+        
+
+        public void Deleteproducts(int id)
+        {
+            var del = entity.Products.Where(x => x.ProductID == id).FirstOrDefault();
+            entity.Entry(del).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+
+        public void Deletewhey(int id)
+        {
+            var del = entity.Products.Where(x => x.ProductID == id).FirstOrDefault();
+            entity.Entry(del).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+        public void DeleteOrders(int id) {
+            var delete = entity.Orders.Where(x => x.OrderID == id).FirstOrDefault();
+            entity.Entry(delete).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+        public void Updatebay(int id)
+        {
+            var userDB = entity.Orders.Where(x => x.OrderID == id).FirstOrDefault();
+            userDB.status_bay = 1;
+
+            entity.Entry(userDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+       
+
+        public void Deletemember(int id)
+        {
+            var del = entity.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
+            entity.Entry(del).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+
+        public void DeleteOrder(int id) {
+            var dels = entity.Orders.Where(x => x.OrderID == id).FirstOrDefault();
+            entity.Entry(dels).State = System.Data.EntityState.Deleted;
+            entity.SaveChanges();
+        }
+
+        public Models.Products Getproduct(int id)
+        {
+            return entity.Products.Where(x => x.ProductID == id).FirstOrDefault();
+        }
+
+        public Models.Customers Getmember(int id)
+        {
+            return entity.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
+        }
+
+       
+
+        public Models.Products GetProducts(int id)
+        {
+            return entity.Products.Where(x => x.ProductID == id).FirstOrDefault();
+        }
+
+        public void Update(Models.Products prod)
+        {
+            var userDB = entity.Products.Where(x => x.ProductID == prod.ProductID).FirstOrDefault();
+            userDB.CategoryID = prod.CategoryID;
+            userDB.ProductName = prod.ProductName;
+            userDB.Stock = prod.Stock;
+            userDB.Price = prod.Price;
+            userDB.Details = prod.Details;
+
+            entity.Entry(userDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+
+       
+
+        public void UpdatProduct(Models.Products why)
+        {
+            var userDB = entity.Products.Where(x => x.ProductID == why.ProductID).FirstOrDefault();
+            userDB.ProductName = why.ProductName;
+            userDB.Price = why.Price;
+            userDB.Stock = why.Stock;
+
+            entity.Entry(userDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+        public void Updatemem(Models.Customers mem)
+        {
+            var userDB = entity.Customers.Where(x => x.CustomerID == mem.CustomerID).FirstOrDefault();
+            userDB.ContactName = mem.ContactName;
+            userDB.Password = mem.Password;
+            userDB.Phone = mem.Phone;
+            userDB.Address = mem.Address;
+           
+            entity.Entry(userDB).State = System.Data.EntityState.Modified;
+            entity.SaveChanges();
+        }
+
+        
+    }
+}
